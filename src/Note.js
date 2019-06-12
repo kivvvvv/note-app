f3b13981a36ab25cbf7c4eba84a49421989512d2
kivvvvv <kivistheslayer@gmail.com>
2019-06-12 19:21:27 +0700

    28# feat: added slide transition on Note

--- src/Note.js
+++ src/Note.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
@@ -8,6 +8,7 @@ import Checkbox from "@material-ui/core/Checkbox";
 import IconButton from "@material-ui/core/IconButton";
 import EditIcon from "@material-ui/icons/Edit";
 import DeleteIcon from "@material-ui/icons/Delete";
+import Slide from "@material-ui/core/Slide";
 import { makeStyles } from "@material-ui/core/styles";
 
 import { deleteNote, toggleNote } from "./reducers/noteActions";
@@ -34,8 +35,12 @@ const useStyles = makeStyles(() => ({
 export default function Note(props) {
   const classes = useStyles(props);
 
+  const [isInSlide, setIsInSlide] = useState(true);
+
   const handleDeleteClick = () => {
-    props.noteDispatch(deleteNote(props.id));
+    Promise.resolve(setIsInSlide(false)).then(() => {
+      setTimeout(() => props.noteDispatch(deleteNote(props.id)), 225);
+    });
   };
 
   const handleEditClick = () => {
@@ -60,48 +65,50 @@ export default function Note(props) {
   };
 
   return (
-    <ListItem
-      button
-      className={`Note ${classes.root}`}
-      onClick={handleViewClick}
-    >
-      <ListItemIcon onClick={handleToggleClick}>
-        <Checkbox edge="start" checked={props.noteCompleted} />
-      </ListItemIcon>
-      <ListItemText
-        primary={
-          <Fragment>
-            <Typography className={classes.noteTitlePrimary}>
-              {getSummaryText(props.noteTitle)}
-            </Typography>
-          </Fragment>
-        }
-        secondary={
-          props.noteCompleted ? null : (
+    <Slide direction="left" in={isInSlide} mountOnEnter unmountOnExit>
+      <ListItem
+        button
+        className={`Note ${classes.root}`}
+        onClick={handleViewClick}
+      >
+        <ListItemIcon onClick={handleToggleClick}>
+          <Checkbox edge="start" checked={props.noteCompleted} />
+        </ListItemIcon>
+        <ListItemText
+          primary={
             <Fragment>
-              <Typography
-                component="span"
-                variant="body1"
-                color="secondary"
-                className={classes.noteTextSecondary}
-              >
-                {getSummaryText(props.noteText)}
-              </Typography>
-              <Typography variant="caption" color="textSecondary">
-                {getLastUpdatedText(props.noteUpdatedAt)}
+              <Typography className={classes.noteTitlePrimary}>
+                {getSummaryText(props.noteTitle)}
               </Typography>
             </Fragment>
-          )
-        }
-      />
-      <ListItemSecondaryAction className={classes.listItemSecondaryAction}>
-        <IconButton edge="end" onClick={handleEditClick}>
-          <EditIcon />
-        </IconButton>
-        <IconButton edge="end" onClick={handleDeleteClick}>
-          <DeleteIcon />
-        </IconButton>
-      </ListItemSecondaryAction>
-    </ListItem>
+          }
+          secondary={
+            props.noteCompleted ? null : (
+              <Fragment>
+                <Typography
+                  component="span"
+                  variant="body1"
+                  color="secondary"
+                  className={classes.noteTextSecondary}
+                >
+                  {getSummaryText(props.noteText)}
+                </Typography>
+                <Typography variant="caption" color="textSecondary">
+                  {getLastUpdatedText(props.noteUpdatedAt)}
+                </Typography>
+              </Fragment>
+            )
+          }
+        />
+        <ListItemSecondaryAction className={classes.listItemSecondaryAction}>
+          <IconButton edge="end" onClick={handleEditClick}>
+            <EditIcon />
+          </IconButton>
+          <IconButton edge="end" onClick={handleDeleteClick}>
+            <DeleteIcon />
+          </IconButton>
+        </ListItemSecondaryAction>
+      </ListItem>
+    </Slide>
   );
 }
